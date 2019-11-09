f51eb1d72ef434736c55417222c255d6fa4be8ad
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-11-09 21:33:16 +0200

    Add some Physics

--- js/bird.js
+++ js/bird.js
@@ -1,45 +1,16 @@
 class Bird{
-    constructor(x, y, w, h, context, src)
+    constructor(x, y, w, h)
     {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.context = context;
-        this.sprite = new Image();
-        this.sprite.src = src;
+        //this.sprite = loadImage(src);
     };
 
-    draw() {
-        this.context.drawImage(this.sprite, this.x, this.y, this.w, this.h);
+    show() {
+        //image(this.sprite, this.x, this.y, this.w, this.h);
+        fill(255);
+        circle(this.x, this.y, this.w);
     }
-
-    step(x, y){
-        if (x > 0 && this.x + this.w < canvas.width || x < 0 && this.x > 0)
-        {
-            if (x > 0)
-                x = Math.min(x, canvas.width - this.x - this.w);
-
-            this.x += x;
-        }
-        if (y > 0 && this.y + this.h < canvas.height || y < 0 && this.y > 0)
-        {
-            if (y > 0)
-                y = Math.min(y, canvas.height - this.y - this.h);
-                
-            this.y += y;
-        }
-    }
-
 }
-
-/*
-pixelsLeft,
-pixelsTop,
-spriteWidth,
-spriteHeight,
-canvasPosX,
-canvasPosY,
-spriteWidth,
-spriteHeight
-*/
--- js/game.js
+++ js/game.js
@@ -1,48 +1,26 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-canvas.tabIndex = 1;
+let ground, box;
+let bird1;
+let backgroundImg;
+let world, engine;
 
-
-
-let bird;
-let background;
-
-bird = new Bird(100, 100, 50, 50, context, "../assets/bird1.png");
-background = new Background(0, 0, canvas.width, canvas.height, context, "../assets/Background.jpg");
-
-canvas.onkeydown = function keyDown(e) {
-    if (e.keyCode == 87 || e.keyCode == 38)   // W key or arrow up
-    {
-        bird.step(0, -20);
-    }
-    if (e.keyCode == 83 || e.keyCode == 40)  // S key or arrow down
-    {
-        bird.step(0, 20);
-    }
-    if (e.keyCode == 68 || e.keyCode == 39)    // D key or arrow right
-    {
-        bird.step(20, 0);
-    }
-    if (e.keyCode == 65 || e.keyCode == 37)   // A key or arrow left
-    {
-        bird.step(-20, 0);
-    }
+function preload() {
+    //backgroundImg = loadImage("../assets/Background.jpg");
 }
 
+function setup() {
+    createCanvas(600, 400);
+    engine = Matter.Engine.create();
+    world = engine.world;
 
-
-function animate() {
-    requestAnimationFrame(animate);
-    background.draw();
-    bird.draw();
-    //bird.step((Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10);
+    ground = new Ground(width / 2, height - 10, width, 20);
+    box = new Box(450, 0, 50, 75); 
+    bird1 = new Bird(50, 300, 25, 50);    
 }
 
-
-animate();
-
-
-
-
-
-
+function draw() {
+    background(0);
+    Matter.Engine.update(engine);
+    ground.show();
+    box.show();
+    bird1.show();
+}
