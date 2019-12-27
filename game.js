d8b75cb1db52a90449793d30c707f834d0bc0351
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-12-27 16:27:20 +0200

    Fix Play Again Option

--- game.js
+++ game.js
@@ -18,17 +18,17 @@ function preload() {
 }
 
 function setup() {
-    const canvas = createCanvas(600, 400);
+    const canvas = createCanvas(800, 600);
     engine = Matter.Engine.create();
     world = engine.world;
 
-    ground = new Ground(width / 2, height - 30, width, 20);
-    for (let i = 0; i < 3; i++) {
+    ground = new Ground(width / 2, height - 70, width, 20);
+    for (let i = 0; i < 4; i++) {
         boxes[i] = new Box(450, 300 - i * 75, 84, 84);
     } 
     bird1 = new Bird(150, 250, 25);    
 
-    slingshot = new SlingShot(150, 250, bird1.body);
+    slingshot = new SlingShot(150, 400, bird1.body);
     
     const mouse = Mouse.create(canvas.elt);
     const options = {
@@ -42,10 +42,14 @@ function setup() {
 
 
 function keyPressed() {
-    if (key == ' ') {
-      World.remove(world, bird.body);
-      bird = new Bird(150, 250, 25);
-      slingshot.attach(bird.body);
+    if (key == ' ') {   // IF SPACE PRESSED PLAY AGAIN
+      for (let i = 0; i < 4; i++) {
+      	World.remove(world, boxes[i].body);
+      	boxes[i] = new Box(450, 300 - i * 75, 84, 84);
+      }
+      World.remove(world, bird1.body);
+      bird1 = new Bird(150, 250, 25);
+      slingshot.attach(bird1.body);
     }  
 }
 
